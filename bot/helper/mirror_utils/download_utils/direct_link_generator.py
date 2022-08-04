4650205fcefe7b01a88db990720a290e8d4f25a9
Wiszky <54537571+vishnoe115@users.noreply.github.com>
2022-08-04 14:17:56 +0700

    100% Fixed Zippyshare

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -136,12 +136,12 @@ def zippy_share(url: str) -> str:
     js_script = str(js_script)
 
     try:
-        mtk = eval(re_findall(r"\+\((.*?).\+", js_script)[0] + " + 11")
+        mtk = eval(re_findall(r"\+.\((.*?)\).\+", js_script)[0])
         uri1 = re_findall(r".href.=.\"/(.*?)/\"", js_script)[0]
-        uri2 = re_findall(r"\)\+\"/(.*?)\"", js_script)[0]
+        uri2 = re_findall(r"\+.\"/(.*?)\"", js_script)[0]
     except Exception as err:
         LOGGER.error(err)
-        raise DirectDownloadLinkException("ERROR: Can't Generate direct link")
+        raise DirectDownloadLinkException("ERROR: Tidak dapat mengambil direct link")
     dl_url = f"{base_url}/{uri1}/{int(mtk)}/{uri2}"
     return dl_url
 
